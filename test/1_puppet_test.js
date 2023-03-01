5a86247564237a9257e6d70c75a2b1de3dc6d7ce
Taylor Y <tdy721@gmail.com>
2023-03-01 12:04:10 -0700

    fmt

--- test/1_puppet_test.js
+++ test/1_puppet_test.js
@@ -1,8 +1,8 @@
-import { assertEquals } from "$std/testing/asserts.ts";
-import { freshPuppetTestWrapper } from "@/test/runner.js";
+import { assertEquals } from "$std/testing/asserts.ts"
+import { freshPuppetTestWrapper } from "@/test/runner.js"
 // import { BASE_URL } from "@/utils/config.js"
 
-const BASE_URL = Deno.env.get("BASE_URL") || "http://localhost:8000";
+const BASE_URL = Deno.env.get("BASE_URL") || "http://localhost:8000"
 
 Deno.test(
   "Public Pages Testing",
@@ -10,17 +10,17 @@ Deno.test(
     await t.step("The homepage should work", async () => {
       const response = await page.goto(`${BASE_URL}`, {
         waitUntil: "networkidle2",
-      });
-      assertEquals(response.status(), 200);
-    });
+      })
+      assertEquals(response.status(), 200)
+    })
 
     await t.step("The 404 page should 404", async () => {
       const response = await page.goto(`${BASE_URL}/404`, {
         waitUntil: "networkidle2",
-      });
-      assertEquals(response.status(), 404);
-    });
+      })
+      assertEquals(response.status(), 404)
+    })
 
     // More steps?
   }),
-);
+)
