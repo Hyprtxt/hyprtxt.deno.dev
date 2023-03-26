545b891909fa7fddfcdcdffa6cf45ae3683448de
Taylor Y <tdy721@gmail.com>
2023-03-26 14:52:14 -0700

    schema json-ld

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,9 +3,52 @@ import Footer from "@/components/Footer.jsx"
 import { tw } from "twind"
 import { DENO_ENV } from "@/utils/config.js"
 import { globalStyles } from "@/utils/style.js"
+import { Head } from "$fresh/runtime.ts"
+
+const SchemaORG = () => {
+  const Schema = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Organization",
+        "@id": "https://hyprtxt.dev/#organization",
+        "url": "https://hyprtxt.dev/",
+        "name": "WordPress",
+        "logo": {
+          "@type": "ImageObject",
+          "@id": "https://hyprtxt.dev/#logo",
+          "url": "https://hyprtxt.dev/logo.png",
+        },
+        "sameAs": [
+          "https://www.facebook.com/Hyprtxt/",
+          "https://twitter.com/Hyprtxt",
+        ],
+      },
+      {
+        "@type": "WebSite",
+        "@id": "https://hyprtxt.dev/#website",
+        "url": "https://hyprtxt.dev/",
+        "name": "hyprtxt.dev",
+        "publisher": {
+          "@id": "https://hyprtxt.dev/#organization",
+        },
+      },
+    ],
+  }
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(Schema, null, 2) }}
+    >
+    </script>
+  )
+}
 
 const Layout = ({ children, data = {} }) => (
   <>
+    <Head>
+      <SchemaORG />
+    </Head>
     <div class={tw`${globalStyles}`}></div>
     <section class={tw`flex justify-center header-wrapper`}>
       <Header />
