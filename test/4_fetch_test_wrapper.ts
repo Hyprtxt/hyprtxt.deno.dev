c925b72542b448e6d694959b01628d69e8a686ad
Taylor Y <tdy721@gmail.com>
2023-08-16 12:26:44 -0700

    labels

--- test/4_fetch_test_wrapper.ts
+++ test/4_fetch_test_wrapper.ts
@@ -1,8 +1,5 @@
-// import { createHandler } from "$fresh/server.ts"
-// import manifest from "@/fresh.gen.ts"
 import { delay } from "$std/async/delay.ts"
 import { startFreshServer } from "$fresh/tests/test_utils.ts"
-
 import { BASE_URL } from "@/utils/config.js"
 import { assert, assertEquals } from "$std/assert/mod.ts"
 import { Status } from "$std/http/http_status.ts"
@@ -20,15 +17,12 @@ const myTestWrapper =
     await delay(100)
   }
 
-export const fixtureTestWrapper = myTestWrapper([
+export const fetchTestWrapper = myTestWrapper([
   "run",
   "-A",
   "--unstable",
   "./main.ts",
 ])
-
-// import { wrapFetch } from "cookiejar";
-// const fetch = wrapFetch();
 
 Deno.test(
   "The index page works",
@@ -36,7 +30,7 @@ Deno.test(
     sanitizeResources: false,
     sanitizeOps: false,
   },
-  fixtureTestWrapper(async (t: Deno.TestContext) => {
+  fetchTestWrapper(async (t: Deno.TestContext) => {
     await t.step("The index page returns a 200 and a 'Welcome'", async () => {
       const response = await fetch(`${BASE_URL}`)
       assertEquals(response.status, Status.OK)
