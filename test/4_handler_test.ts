b261e913bc0c3ea4f73ef339d289ba08dbfca408
Taylor Y <tdy721@gmail.com>
2023-08-16 12:16:33 -0700

    typescript

--- test/4_handler_test.ts
+++ test/4_handler_test.ts
@@ -7,17 +7,18 @@ import { BASE_URL } from "@/utils/config.js"
 import { assert, assertEquals } from "$std/assert/mod.ts"
 import { Status } from "$std/http/http_status.ts"
 
-const myTestWrapper = (args: any) => (theTests: any) => async (t: any) => {
-  const { serverProcess, lines } = await startFreshServer({
-    args,
-  })
-  await theTests(t)
-  // Stop the Server
-  await lines.cancel()
-  serverProcess.kill("SIGTERM")
-  // await for the server to close
-  await delay(100)
-}
+const myTestWrapper =
+  (args: string[]) => (theTests: any) => async (t: Deno.TestContext) => {
+    const { serverProcess, lines } = await startFreshServer({
+      args,
+    })
+    await theTests(t)
+    // Stop the Server
+    await lines.cancel()
+    serverProcess.kill("SIGTERM")
+    // await for the server to close
+    await delay(100)
+  }
 
 export const fixtureTestWrapper = myTestWrapper([
   "run",
@@ -35,8 +36,8 @@ Deno.test(
     sanitizeResources: false,
     sanitizeOps: false,
   },
-  fixtureTestWrapper(async (t: any) => {
-    await t.step("The index page returns a 200 and 'Welcome'", async () => {
+  fixtureTestWrapper(async (t: Deno.TestContext) => {
+    await t.step("The index page returns a 200 and a 'Welcome'", async () => {
       const response = await fetch(`${BASE_URL}`)
       assertEquals(response.status, Status.OK)
       const text = await response.text()
